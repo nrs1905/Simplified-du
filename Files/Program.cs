9d47b193735ae4a9b344f9a4b4ecd4250ff1fe77
nrs1905 <nrs1905@rit.edu>
2024-02-20 13:52:29 -0500

    Removed the commmented out help message

--- Files/Program.cs
+++ Files/Program.cs
@@ -40,12 +40,3 @@ namespace filesearch
     }
     }
 }
-/*
-Usage: du[-s][-d][-b] < path >
-Summarize disk usage of the set of FILES, recursively for directories.
-You MUST specify one of the parameters, -s, -d, or -b
--s Run in single threaded mode
--d Run in parallel mode (uses all available processors)
--b Run in both parallel and single threaded mode.
-Runs parallel followed by sequential mode
-*/
